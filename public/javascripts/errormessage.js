d0a248f0ecf95e48b264783adc8f655aa1135a4b
Ian Johnson <killjoy878@gmail.com>
2018-11-28 23:29:29 -0700

    Changed error message time

--- public/javascripts/errormessage.js
+++ public/javascripts/errormessage.js
@@ -3,7 +3,7 @@ $( function () {
   var errorDisplayed = false;
   var errorMessages = ["Tire Pressure Low","Dead Battery","Gas Cap Issues","Starter Issues"];
   var min = 0, max = 3;
-  var errorInterval = 1;
+  var errorInterval = 5000;
 
   window.setInterval( function() {
     if( errorDisplayed == false ) {
